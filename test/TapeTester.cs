ebd13bc6fdc11c8b5acf9307dae3534f67849a31
Timothy Jones <tescosquirrel@gmail.com>
2010-09-23 17:16:27 +1200

    Removing binary files... again.

--- test/TapeTester.cs
+++ test/TapeTester.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using NUnit.Framework;
+using tape.data;
+using tape.io;
+using tape.pipeline;
 
 namespace test {
 
+  [TestFixture]
   public class TapeTester {
+
+    [Test]
+    public void TestPipeline() {
+      AudioReader reader = new AudioReader();
+      SoundData audio = reader.ReadSoundFile("../../../data/file-system.wav");
+      SoundData[] chunks = new AmplitudeAnalyzer().SplitChunks(audio);
+      Assert.AreEqual(2, chunks.Length, "Two chunks are in the file.");
+    }
 
     public static void Main() {}
 
